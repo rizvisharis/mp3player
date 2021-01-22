c6b32bd1f122efa5cf2812d6e0a6e3d1bde66fc7
Rizvi <rizvisharis@gmail.com>
2021-01-22 22:19:01 +0530

    intit the project and import modules

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import tkinter as tkr
+from tkinter.filedialog import askdirectory
+import os
+
+music_player = tkr.Tk()
+music_player.title("Music Player")
+music_player.geometry("450 X 350")
